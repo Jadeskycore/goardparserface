d943cc9d391144b4fd70c77d72b45800eca47972
Andrii Kulikov <jadecoresky@gmail.com>
2017-06-24 18:54:18 +0300

    Added webm component and nice rendering

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ class App extends Component {
     }
     handler(data) {
         this.setState({
+            webms: []
+        });
+        this.setState({
             webms: data.files
         });
     }
--- src/components/WebmTile.js
+++ src/components/WebmTile.js
@@ -1,10 +1,50 @@
 import React from 'react';
 
 export class WebmTile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {webmShow: false};
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange() {
+        this.setState({webmShow: !this.state.webmShow});
+    }
     render() {
+        let webm = null;
+
+        if (this.state.webmShow) {
+            webm = <Webm webmObj={this.props.webmObj}/>
+        } else {
+            webm = null
+        }
         return (
             <div>
-                <img src={'https://2ch.hk/' + this.props.webmObj.thumbnail} alt="stuff"/>
+                <div onClick={this.handleChange}>
+                    <img src={'https://2ch.hk/' + this.props.webmObj.thumbnail} alt="stuff"/>
+                </div>
+                {webm}
+            </div>
+        )
+    }
+}
+
+class Webm extends React.Component {
+    render() {
+        return(
+            <div>
+                <video
+                    controls="controls"
+                    width="50%"
+                    height="50%"
+                >
+                    <source
+                        type="video/webm"
+                        src={"https://2ch.hk/" + this.props.webmObj.path}
+                    >
+
+                    </source>
+                </video>
             </div>
         )
     }
